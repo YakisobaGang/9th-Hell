6cf335264c4a865620283fe0f9dfcf45cc58bc5f
Fabricio Duarte <fabriciohod@hotmail.com>
2021-03-15 15:20:34 -0300

    Atualizando o projeto para a Unity 2020.3.0f1 (LTS)

---
+++ Project_D/Assets/Scripts/Commands/CommandSander.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProjectD.Interfaces;
+
+namespace ProjectD.Commands
+{
+    public class CommandSander : ICommand
+    {
+        private readonly Action commandToBeExec;
+        public CommandSander(Action commandToBeExec, CommandHandler commandHandler)
+        {
+            this.commandToBeExec = commandToBeExec;
+            commandHandler.AddCommand(this);
+        }
+
+        public void Execute()
+        {
+            commandToBeExec();
+        }
+    }
+}
