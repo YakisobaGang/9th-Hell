5403d05e1e13e917fd363c9a843930ececc9a05d
Fabricio Duarte <fabriciohod@hotmail.com>
2021-04-09 14:03:47 -0300

    Adiconado script LoadSceneByIndexAfterTime

---
+++ Project_D/Assets/Scripts/Dialogue/LoadSceneByIndexAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectD.Dialogue
+{
+    public class LoadSceneByIndexAfterTime : MonoBehaviour
+    {
+        [SerializeField] private float timeInSeconds;
+        public void LoadScene(int sceneIndex)
+        {
+            StartCoroutine(CLoadSceneAfterTime(sceneIndex));
+        }
+
+        private IEnumerator CLoadSceneAfterTime(int sceneIndex)
+        {
+            yield return new WaitForSeconds(timeInSeconds);
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
