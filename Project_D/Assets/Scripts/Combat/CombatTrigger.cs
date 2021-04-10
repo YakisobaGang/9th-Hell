7d90abd20adf2678c8d574e4643e2f639e37710d
Fabricio Duarte <fabriciohod@hotmail.com>
2021-04-10 10:05:31 -0300

    Salvando ultima posição do player

---
+++ Project_D/Assets/Scripts/Combat/CombatTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectD.Combat
+{
+    public class CombatTrigger : MonoBehaviour
+    {
+        [SerializeField] private int combatSceneIndex;
+        [SerializeField] private float timeToChangeScene;
+        private bool canLoadScene = false;
+
+        public void ChangeScene()
+        {
+            StartCoroutine(WaitTimeToChangeScene());
+        }
+        
+        private IEnumerator LoadSceneAsync()
+        {
+            var asyncLoad = SceneManager.LoadSceneAsync(combatSceneIndex);
+
+            while (!asyncLoad.isDone && canLoadScene)
+            {
+               yield return null;
+            }
+        }
+
+        private IEnumerator WaitTimeToChangeScene()
+        {
+            yield return new WaitForSeconds(timeToChangeScene);
+            yield return LoadSceneAsync();
+            canLoadScene = true;
+        }
+    }
+}
