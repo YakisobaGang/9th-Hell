5120f6640f930f5f605dee9e872085b7f1d7927c
Fabricio Duarte <fabriciohod@hotmail.com>
2021-04-10 09:08:20 -0300

    Salvando ultima posição do player

---
+++ Project_D/Assets/Scripts/Player/SavePlayerPosition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace ProjectD.Player
+{
+    public class SavePlayerPosition : MonoBehaviour
+    {
+        private string playerX = "PlayerPositionX";
+        private string playerY = "PlayerPositionX";
+        private string playerZ = "PlayerPositionX";
+        private void Awake()
+        {
+            if (PlayerPrefs.GetFloat(playerX) == 0) return;
+            
+            var lastPosition = new Vector3
+            (
+                PlayerPrefs.GetFloat(playerX),
+                PlayerPrefs.GetFloat(playerY),
+                PlayerPrefs.GetFloat(playerZ)
+            );
+
+            transform.position = lastPosition;
+        }
+
+        public void SavePlayerLastPosition()
+        {
+            var position = transform.position;
+            
+            var (x, y, z) = (position.x, position.y, position.z);
+            
+            PlayerPrefs.SetFloat(playerX, x);
+            PlayerPrefs.SetFloat(playerY, y);
+            PlayerPrefs.SetFloat(playerZ, z);
+        }
+    }
+}
