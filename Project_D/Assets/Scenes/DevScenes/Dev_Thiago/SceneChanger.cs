3246cd7ac1f64c4a3645ff0eb1ed8c95a36bd2a0
Thiduba <73235337+Thiduba@users.noreply.github.com>
2021-05-11 14:55:09 -0300

    Main Menu

---
+++ Project_D/Assets/Scenes/DevScenes/Dev_Thiago/SceneChanger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace ProjectD
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        public Animator transition;
+        public Animator start;
+        public float transitionDuration = 1f;
+        
+        public void LoadScene()
+        {
+            StartCoroutine(TransitionTime(1));
+        }
+
+        public void QuitGame()
+        {
+            StartCoroutine(TransitionExit(1));
+        }
+
+        IEnumerator TransitionTime(int arg) 
+        {
+            transition.SetTrigger("Start");
+
+            yield return new WaitForSeconds(transitionDuration);
+
+            SceneManager.LoadScene(1);
+        }
+
+        IEnumerator TransitionExit(int arg)
+        {
+            transition.SetTrigger("Start");
+
+            yield return new WaitForSeconds(transitionDuration);
+
+            Application.Quit();
+        }
+    }
+}
