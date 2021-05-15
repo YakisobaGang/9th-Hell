fdfe2e126cbc1214d862c3c5d7058cd077e00412
Fabricio Duarte <fabriciohod@hotmail.com>
2021-05-15 10:46:31 -0300

    Removendo um print (Debug.Log)

--- Project_D/Assets/Scripts/Player/PlayerAnimController.cs
+++ Project_D/Assets/Scripts/Player/PlayerAnimController.cs
@@ -10,6 +10,7 @@ namespace ProjectD
         private static readonly int Horizontal = Animator.StringToHash("Horizontal");
         private static readonly int Vertical = Animator.StringToHash("Vertical");
         private static readonly int isWalking = Animator.StringToHash("IsWalking");
+        
 
         private void Awake()
         {
@@ -28,8 +29,6 @@ namespace ProjectD
 
         private void HandlePlayerMoveDir(Vector2 dir)
         {
-            print($"X: {dir.x :N1} | Y: {dir.y :N1}");
-            
             if ((dir.x, dir.y) == (0, 0))
             {
                 anim.SetBool(isWalking, false);
