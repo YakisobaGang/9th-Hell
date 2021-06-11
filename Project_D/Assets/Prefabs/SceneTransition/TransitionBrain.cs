be70d17087ae5200fcc0fc3f5a49d8d3c0a09413
Thiago <thiduba@gmail.com>
2021-06-10 23:31:14 -0300

    Transições
    PreFab de transições
    Nova cena MainMenu

---
+++ Project_D/Assets/Prefabs/SceneTransition/TransitionBrain.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectD
+{
+    public class TransitionBrain : MonoBehaviour
+    {
+        public Animator anim;
+        public float transitionTime = 1f;
+
+        public void LoadNextLevel()
+        {
+            StartCoroutine(LoadingLevel((SceneManager.GetActiveScene().buildIndex + 1)));
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        IEnumerator LoadingLevel(int levelIndex)
+        {
+            anim.SetTrigger("Start");
+            yield return new WaitForSeconds(transitionTime);
+            SceneManager.LoadScene(levelIndex);
+        }
+    }
+}
