2ac6694b736f7aa1efc7cbb091870e028dbeb929
Fabricio Duarte <fabriciohod@hotmail.com>
2021-06-12 09:40:05 -0300

    Montando cenario

---
+++ Project_D/Assets/Scripts/Utils/DrawnBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectD.Utils
+{
+#if UNITY_EDITOR
+    public class DrawnBox : MonoBehaviour
+    {
+        [SerializeField] private Vector3 size;
+        [SerializeField] private Color color;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = color;
+            Gizmos.DrawCube(transform.position, size);
+        }
+    }
+#endif
+}
