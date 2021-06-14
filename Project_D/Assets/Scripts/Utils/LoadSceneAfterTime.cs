5df68de81192ce3f496a89fcc582b55c0456b4cc
Fabricio Duarte <fabriciohod@hotmail.com>
2021-06-14 01:05:46 -0300

    Build de teste

---
+++ Project_D/Assets/Scripts/Utils/LoadSceneAfterTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectD.Utils
+{
+    public class LoadSceneAfterTime : MonoBehaviour
+    {
+        [SerializeField] private float time;
+        [SerializeField] private int sceneIndex;
+
+        public void LoadScene()
+        {
+            StartCoroutine(nameof(C_loadScene));
+        }
+
+        IEnumerator C_loadScene()
+        {
+            yield return new WaitForSeconds(time);
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
